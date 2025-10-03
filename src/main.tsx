19791adf84cc7b9c3ae748adef7de356c33a43b7
ynandasr <ynandasari@gmail.com>
2025-10-04 01:02:56 +0700

    first deploy portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; 
 import App from './App.tsx';
 import './index.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
